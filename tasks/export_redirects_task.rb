a12e7209356c71c8065df781513743f8857a68a8
Ben Sheldon <bensheldon@gmail.com>
2014-02-19 22:31:29 -0800

    add a task to export redirections for disqus

---
+++ tasks/export_redirects_task.rb
@@ -0,0 +1,33 @@
+require "middleman-core/cli"
+
+class ExportRedirects < Thor
+  include Thor::Actions
+
+  namespace :export_redirects
+
+  desc "export_redirects", "Synchronise your Middleman build to S3 and more"
+  def export_redirects
+    app = ::Middleman::Application.server.inst
+    text = ""
+    host = "island94.org"
+
+    app.data.global_redirects.each do |target, sources|
+    Array(sources).each do |source|
+      text << "http://#{host}/#{source}, http://#{host}/#{target}\n"
+      text << "http://www.#{host}/#{source}, http://www.#{host}/#{target}\n"
+    end
+  end
+
+    app.blog.articles.each do |article|
+      article.data.fetch('redirects', []).each do |source|
+        target = article.request_path
+
+        text << "http://#{host}/#{source}, http://#{host}/#{target}\n"
+        text << "http://www.#{host}/#{source}, http://www.#{host}/#{target}\n"
+      end
+    end
+
+    create_file "redirections.txt", text
+  end
+
+end
